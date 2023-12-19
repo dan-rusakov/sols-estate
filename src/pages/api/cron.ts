304af4d81b0eee9067899be2fd0662cb8495c239
Daniel Rusakov <dev.rusakov@gmail.com>
2023-12-19 21:00:40 +0700

    feat: delete declarations after 7 days

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -6,43 +6,52 @@ import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 import { ISO_DATE_FORMAT } from "~/utils/date";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        const context = await createTRPCContext({
-            req,
-            res
-        });
-        const caller = appRouter.createCaller(context);
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  try {
+    const context = await createTRPCContext({
+      req,
+      res,
+    });
+    const caller = appRouter.createCaller(context);
 
-        const dateThreeDaysBeforeCurrent = dayjs().subtract(3, 'day').format(ISO_DATE_FORMAT);
-        const declarations = await caller.declarations.getAllDeclarations({
-            priceMin: null,
-            priceMax: null,
-            roomsMin: null,
-            roomsMax: null,
-            districtSlug: null,
-            propertyTypeSlug: null,
-            page: null,
-            createdAtMax: dateThreeDaysBeforeCurrent,
-        });
-        const declarationIds = declarations?.data[1].map(declaration => declaration.id);
-        const deleteDeclarationPromises = declarationIds.map(declarationId => caller.declarations.removeDeclaration({
-            declarationId,
-        }));
+    const dateSevenDaysBeforeCurrent = dayjs()
+      .subtract(7, "day")
+      .format(ISO_DATE_FORMAT);
+    const declarations = await caller.declarations.getAllDeclarations({
+      priceMin: null,
+      priceMax: null,
+      roomsMin: null,
+      roomsMax: null,
+      districtSlug: null,
+      propertyTypeSlug: null,
+      page: null,
+      createdAtMax: dateSevenDaysBeforeCurrent,
+    });
+    const declarationIds = declarations?.data[1].map(
+      (declaration) => declaration.id,
+    );
+    const deleteDeclarationPromises = declarationIds.map((declarationId) =>
+      caller.declarations.removeDeclaration({
+        declarationId,
+      }),
+    );
 
-        await Promise.all(deleteDeclarationPromises);
+    await Promise.all(deleteDeclarationPromises);
 
-        res.status(200).json({ data: { status: 'success' } });
-    } catch (cause) {
-        if (cause instanceof TRPCError) {
-            const httpStatusCode = getHTTPStatusCodeFromError(cause);
+    res.status(200).json({ data: { status: "success" } });
+  } catch (cause) {
+    if (cause instanceof TRPCError) {
+      const httpStatusCode = getHTTPStatusCodeFromError(cause);
 
-            res.status(httpStatusCode).json({ error: { message: cause.message } });
-            return;
-        }
-
-        res.status(500).json({
-            error: { message: `Error while deleting declaration` },
-        });
+      res.status(httpStatusCode).json({ error: { message: cause.message } });
+      return;
     }
+
+    res.status(500).json({
+      error: { message: `Error while deleting declaration` },
+    });
+  }
 }
